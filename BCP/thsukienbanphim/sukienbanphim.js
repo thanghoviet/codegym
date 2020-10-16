2c007b7eb7e57a5306ec6a08103dccb2233ae217
thang.hoviet <hovietthang1998@gmail.com>
2020-10-16 09:50:54 +0700

    sudungbanphim

---
+++ BCP/thsukienbanphim/sukienbanphim.js
@@ -0,0 +1,31 @@
+let element = document.getElementById("nobita");
+
+function leftArrowPressed() {
+  element.style.left = parseInt(element.style.left) - 5 + "px";
+}
+function rightArrowPressed() {
+  element.style.left = parseInt(element.style.left) + 5 + "px";
+}
+function upArrowPressed() {
+  element.style.top = parseInt(element.style.top) - 5 + "px";
+}
+function downArrowPressed() {
+  element.style.top = parseInt(element.style.top) + 5 + "px";
+}
+function moveSelection(evt) {
+  switch (evt.keyCode) {
+    case 37:
+      leftArrowPressed();
+      break;
+    case 39:
+      rightArrowPressed();
+      break;
+    case 38:
+      upArrowPressed();
+      break;
+    case 40:
+      downArrowPressed();
+      break;
+  }
+}
+window.addEventListener("keydown", moveSelection);
